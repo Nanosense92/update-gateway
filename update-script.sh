54b38bdd28982044269e6fbf0b960bd8f38bd417
root <root@raspberrypi>
2019-09-09 14:21:40 +0000

    fix bug

--- update-script.sh
+++ update-script.sh
@@ -3,13 +3,11 @@
 echo "updating the gateway" >> update_log.log;
 
 sudo cp -rf /home/pi/enocean-gateway /tmp 
-sudo rm -rf /home/pi/enocean-gateway
 sudo cp -rf enocean-gateway /home/pi
 
 echo "updating script and routine" >> update_log.log;
 
 sudo cp -rf /var/www/html/nanosense /tmp
-sudo rm -rf /var/www/html/nanosense
 sudo cp -rf nanosense /var/www/html
 
 echo "updating web-interface" >> update_log.log;
