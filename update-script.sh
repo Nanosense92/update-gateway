bc1ffad8c080cf573d24c75fa180da93675f261f
root <root@raspberrypi>
2019-09-09 12:05:36 +0000

    testing

--- update-script.sh
+++ update-script.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
 echo "updating the gateway" >> update_log.log;
+
+sudo mv -f enocean-gateway /home/pi
+
+echo "updating script and routine" >> update_log.log;
+
+sudo mv -f nanosense /var/www/html
+
+echo "updating web-interface" >> update_log.log;
