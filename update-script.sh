99c22fc4f15c66aa199215add472e424e0dc5607
root <root@raspberrypi>
2019-09-10 08:30:09 +0000

    change dir

--- update-script.sh
+++ update-script.sh
@@ -2,12 +2,12 @@
 
 echo "updating the gateway" >> update_log.log;
 
-sudo cp -rf /home/pi/enocean-gateway /tmp 
+sudo cp -rf /home/pi/enocean-gateway /home/pi/backup 
 sudo cp -rf enocean-gateway /home/pi
 
 echo "updating script and routine" >> update_log.log;
 
-sudo cp -rf /var/www/html/nanosense /tmp
+sudo cp -rf /var/www/html/nanosense /home/pi/backup
 sudo cp -rf nanosense /var/www/html
 
 echo "updating web-interface" >> update_log.log;
