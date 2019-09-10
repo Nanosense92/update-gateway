3f62dad950d0b885a64de5b2939686f8ffc2f793
root <root@raspberrypi>
2019-09-10 09:39:52 +0000

    test

--- update-script.sh
+++ update-script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DIFF = $(diff version-update.txt /home/pi/backup/backup_version.txt)
+DIFF=$(diff version-update.txt /home/pi/backup/backup_version.txt)
 if ["$DIFF" != ""]
 then
 
@@ -17,4 +17,7 @@ then
 	echo "updating web-interface" >> update_log.log;
 
 	sudo echo $(cat version-update.txt) > /home/pi/backup/backup_version.txt
+else
+	echo "Already to the Newest version" >> update_log.log;
+
 fi
