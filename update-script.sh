5d3bad6ccdc52a15e484b688a0f84b0b6acb30c1
root <root@raspberrypi>
2019-09-10 09:46:36 +0000

    path

--- update-script.sh
+++ update-script.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-DIFF=$(diff version-update.txt /home/pi/backup/backup_version.txt)
+DIFF=$(diff update-gateway/version-update.txt /home/pi/backup/backup_version.txt)
 if ["$DIFF" != ""]
 then
 
 	echo "updating the gateway" >> update_log.log;
 
 	sudo cp -rf /home/pi/enocean-gateway /home/pi/backup 
-	sudo cp -rf enocean-gateway /home/pi
+	sudo cp -rf update-gateway/enocean-gateway /home/pi
 
 	echo "updating script and routine" >> update_log.log;
 
 	sudo cp -rf /var/www/html/nanosense /home/pi/backup
-	sudo cp -rf nanosense /var/www/html
+	sudo cp -rf update-gateway/nanosense /var/www/html
 
 	echo "updating web-interface" >> update_log.log;
 
-	sudo echo $(cat version-update.txt) > /home/pi/backup/backup_version.txt
+	sudo echo $(cat update-gateway/version-update.txt) > /home/pi/backup/backup_version.txt
 else
 	echo "Already to the Newest version" >> update_log.log;
 
