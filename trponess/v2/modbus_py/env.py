1bdc8c1f07d41da33f1984889a61ed459bd58d87
Robert <robert@nano-sense.com>
2020-03-24 16:52:28 +0000

    v2 added session.py changed env

--- trponess/v2/modbus_py/env.py
+++ trponess/v2/modbus_py/env.py
@@ -11,6 +11,7 @@ class Env:
     modbuscachefile = target + '/modbus__cache/modbus_cache.ini'
     logfile = target + '/modbus__cache/log.ini'
     sessionfile= target + '/modbus__cache/session.ini'
+    
 
     @staticmethod
     def create_dir(adir):
@@ -18,16 +19,18 @@ class Env:
             os.mkdir(adir)
             
     @staticmethod
-    def create_file(afile):
-        open(afile, mode='w+').close()
+    def create_file(afile, mode):
+        open(afile, mode=mode).close()
+        os.chmod(afile, 0o777)
 
     @staticmethod
     def setup_env():
         Env.create_dir(Env.cachedir)
-        Env.create_file(Env.notfoundfile)
-        Env.create_file(Env.datafile)
-        Env.create_file(Env.modbuscachefile)
-        Env.create_file(Env.logfile)
+        Env.create_file(Env.notfoundfile, '+w')
+        Env.create_file(Env.datafile, '+w')
+        Env.create_file(Env.modbuscachefile, '+w')
+        Env.create_file(Env.logfile, '+w')
+        Env.create_file(Env.sessionfile, '+a')
 
   
 if __name__ == "__main__":
