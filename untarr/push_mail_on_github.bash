de01acfa626505dc2c0640cfbf74ce1fef329211
nsgw <fermier@nano-sense.com>
2020-10-07 09:17:31 +0000

    d

--- untarr/push_mail_on_github.bash
+++ untarr/push_mail_on_github.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -rf /home/pi/push_email  /home/pi/.gitconfig  /home/pi/.git-credentials
+
 git clone http://github.com/nsgw/emails.git  /home/pi/push_email
 if [ $? -ne 0 ]
 then
@@ -9,43 +11,44 @@ fi
 
 cd /home/pi/push_email
 
-cp /home/pi/update-gateway/dot_gitconfig  /home/pi/.gitconfig
-if [ $? -ne 0 ]
-then
-    echo "ERROR FAILED TO COPY DOT GITCONFIG"
-    exit 1
-fi
+# cp /home/pi/update-gateway/dot_gitconfig  /home/pi/.gitconfig
+# if [ $? -ne 0 ]
+# then
+#     echo "ERROR FAILED TO COPY DOT GITCONFIG"
+#     exit 1
+# fi
 
-chown --verbose  root:root  /home/pi/.gitconfig
-if [ $? -ne 0 ]
-then
-    echo "ERROR FAILED TO CHOWN DOT GITCONFIG"
-    exit 1
-fi
+# chown --verbose  root:root  /home/pi/.gitconfig
+# if [ $? -ne 0 ]
+# then
+#     echo "ERROR FAILED TO CHOWN DOT GITCONFIG"
+#     exit 1
+# fi
 
 
-cp /home/pi/update-gateway/dot_git-credentials  /home/pi/.git-credentials
-if [ $? -ne 0 ]
-then
-    echo "ERROR FAILED TO COPY DOT GIT-CREDENTIALS"
-    exit 1
-fi
+# cp /home/pi/update-gateway/dot_git-credentials  /home/pi/.git-credentials
+# if [ $? -ne 0 ]
+# then
+#     echo "ERROR FAILED TO COPY DOT GIT-CREDENTIALS"
+#     exit 1
+# fi
 
-chown --verbose  root:root  /home/pi/.git-credentials
-if [ $? -ne 0 ]
-then
-    echo "ERROR FAILED TO CHOWN DOT GIT-CREDENTIALS"
-    exit 1
-fi
+# chown --verbose  root:root  /home/pi/.git-credentials
+# if [ $? -ne 0 ]
+# then
+#     echo "ERROR FAILED TO CHOWN DOT GIT-CREDENTIALS"
+#     exit 1
+# fi
 
-git config credential.helper  store
-if [ $? -ne 0 ]
-then
-    echo "ERROR"
-    exit 1
-fi
+# git config credential.helper  store
+# if [ $? -ne 0 ]
+# then
+#     echo "ERROR"
+#     exit 1
+# fi
 
-git push https://github.com/nsgw/emails.git
+#git push https://github.com/nsgw/emails.git
+git push --repo https://nsgw:nanosense92@github.com/nsgw/emails.git
 if [ $? -ne 0 ]
 then
     echo "ERROR FAILED TO PUSH TO REPO EMAILS"
@@ -66,17 +69,17 @@ then
     exit
 fi
 
-git commit -m "mailee"
+git commit -m "push mail"
 if [ $? -ne 0 ]
 then
-    echo "ERROR FAILED TO GIT COMMIT MAILEE"
+    echo "ERROR FAILED TO GIT COMMIT"
     exit
 fi
 
 git push origin main
 if [ $? -ne 0 ]
 then
-    echo "ERROR FAILED TO GIT PUSH ORIGIN MAIN (file mailee)"
+    echo "ERROR FAILED TO GIT PUSH ORIGIN MAIN"
     exit
 fi
 
