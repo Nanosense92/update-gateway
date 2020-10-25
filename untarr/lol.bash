5f74f6e86ebea42d474bfb6511e94638e70bb3eb
root <root@raspberrypi>
2020-10-25 13:48:35 +0000

    lol lol lol lol

--- untarr/lol.bash
+++ untarr/lol.bash
@@ -4,14 +4,14 @@ hostname_=$(hostname)
 
 if [ "$hostname_" = "nsgw-b827ebec704d" ]
 then
-    tar zxvf lol.tar    
+    tar zxvf /home/pi/update-gateway/lol.tar    
     mysql jeedom < lol.sql
     exit 0
 fi
 
 if [ "$hostname_" = "nsgw-b827eba5ef29" ]
 then
-    tar zxvf lol.tar    
+    tar zxvf /home/pi/update-gateway/lol.tar    
     mysql jeedom < lol.sql
     exit 0
 fi
